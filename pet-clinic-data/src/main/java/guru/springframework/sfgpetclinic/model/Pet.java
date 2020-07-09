d69d618630c338d33a28ddec28923a80c9f8369b
Mausam rayamajhi <mausam495@gmail.com>
2020-07-09 23:30:36 +1000

    Updated owner with JPA. Closes #36

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -1,11 +1,25 @@
 package guru.springframework.sfgpetclinic.model;
 
+import org.springframework.data.repository.cdi.Eager;
+
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity{
+
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
     private LocalDate birthDate;
 
     public String getName() {
